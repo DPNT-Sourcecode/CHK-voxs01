1178c7afac37f2c3b63dd89ce4517a01cd5aeba5
sbx_user1051 <sbx_user1051@169.254.54.77>
2024-07-27 20:00:29 +0000

    Sat Jul 27 20:00:29 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,12 +2,46 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         if (skus == null || skus.isBlank()) {
             return 0;
         }
 
-        return 0;
+        Map<Character, Integer> productByQuantity = new HashMap<>();
+        for (int i = 0; i < skus.length(); i++) {
+            char item = skus.charAt(i);
+            productByQuantity.merge(item, 0, (value, j) -> value + 1);
+        }
+
+        int total = 0;
+        for (Map.Entry<Character, Integer> entry : productByQuantity.entrySet()) {
+            Character item = entry.getKey();
+            Integer quantity = entry.getValue();
+
+            switch (item) {
+                case 'A':
+                    total += quantity * 50;
+                    break;
+                case 'B':
+                    total += quantity * 30;
+                    break;
+                case 'C':
+                    total += quantity * 20;
+                    break;
+                case 'D':
+                    total += quantity * 15;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return total;
     }
 }
+
